620cd18246d562190c3b735159a3b734c688b98e
Jacob <Jacob.SH@icloud.com>
2021-05-18 15:24:31 +0200

    first commit

---
+++ src/main/java/com/grupp3/article_finder/ArticleFinderApplication.java
@@ -0,0 +1,12 @@
+package com.grupp3.article_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleFinderApplication.class, args);
+	}
+}
---
+++ src/test/java/com/grupp3/article_finder/ArticleFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp3.article_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

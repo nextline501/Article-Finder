d0a8144694cc2bb60f7bd612e8e6fcff7db5cef9
Andreas <andreaskvam@hotmail.com>
2021-05-24 14:37:35 +0200

    Cahnges in frontend

--- frontend/Article Finder/src/components/Navbar.vue
+++ frontend/Article Finder/src/components/Navbar.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="">
-      <img src="../../public/logo-dark.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
+      <img src="/logo-dark.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
       Article Finder
     </a>
   </div>

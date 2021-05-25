a1f916f4d9eab7fb6d2dc65f44b90b4fedd34623
Robert Nilsson <robert.nilsson.jobb@gmail.com>
2021-05-25 19:44:38 +0200

    Added get_summary

--- NLP/summary.py
+++ NLP/summary.py
@@ -37,3 +37,39 @@ sanitizers, toilet rolls people started panic buying which created some problems
 On the other side, social media is also the source of misinformation. Especially, in the case of
 coronavirus such news can be proven chaotic. Spreading this sort of news further without any
 research can be problematic in that panic situation (Logan Godfrey, 2020)."""
+
+
+def get_summary(article, limit):
+    # Remove line breaks
+    clean_text = article.replace("\n", " ")
+    # Transform to doc object for spacy
+    doc = nlp(clean_text)
+    # Create list of keywords
+    keyword =[]
+    for token in doc:
+        if (token.is_stop or token.is_punct):
+            continue
+        if (token.pos_ in ['PROPN','NOUN','ADJ','VERB']):
+            keyword.append(token.text)
+
+    # Ranking of keywords
+    freq_word = Counter(keyword)
+    max_freq = Counter(keyword).most_common(1)[0][1]
+    for word in freq_word.keys():
+        freq_word[word] = (freq_word[word]/max_freq)
+    # Assign weight to the sentences
+    sent_weight ={}
+    for sent in doc.sents:
+        for word in sent:
+            if word.text in freq_word.keys():
+                if sent in sent_weight.keys():
+                    sent_weight[sent] += freq_word[word.text]
+                else:
+                    sent_weight[sent] = freq_word[word.text]
+
+    # Get the sentences with highest weigth - "limit" - integer that gives number of sentences
+    summerized_sents = nlargest(limit, sent_weight, key=sent_weight.get)
+    # Joining the list of sentence to a string
+    final_sents = [ word.text for word in summerized_sents]
+    summary = " ".join(final_sents)
+    return summary

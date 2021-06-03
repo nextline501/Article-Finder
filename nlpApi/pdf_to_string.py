c7a03aa923e9656d4b2d5199fd1b7dd31d9aadf3
Robert Nilsson <robert.nilsson.jobb@gmail.com>
2021-06-03 18:38:04 +0200

    Changed to a better pdf parser

--- nlpApi/pdf_to_string.py
+++ nlpApi/pdf_to_string.py
@@ -1,31 +1,45 @@
-import PyPDF2 
 import urllib.request
 import os
-import pdfplumber
+import pdfminer
+
+from io import StringIO
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
+from pdfminer.pdfparser import PDFParser
+
+
+
 
 # New entry in DB
 # The idea is to send a title and pdf-path as obj from Vue --> Spring --> Sanic --> Spacy (title + generated: text tokentree summary) --> Spring --> DB
 
 def download_file(download_url, filename):
-    
+
     response = urllib.request.urlopen(download_url)    
     file = open(filename + ".pdf", 'wb')
     file.write(response.read())
     file.close()
 
 
-def pdf_to_string(pdfReader):
+def pdf_to_string():
 
-    page_content=""               
-    with pdfplumber.open('test.pdf') as pdf:
+    output_string = StringIO()
+    with open('test.pdf', 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
 
-        for page_number in range(pdfReader.numPages):
-            page = pdf.pages[page_number]
-            page_content += page.extract_text()  
-        page_content = page_content.replace("\n", " ") 
-
-    print(page_content)
-    return page_content
+    text = output_string.getvalue()
+    text = text.replace("\n", " ")
+    print(text)
+    return text
 
 
 def delete_created_file():
@@ -34,19 +48,15 @@ def delete_created_file():
     else:
         print("The file does not exist")
 
-
+# Run this! Returns a 'str'
 def pdf_to_string_process(path):
 
     download_file(path,"test")
-    pdfFileObj = open('test.pdf', 'rb')
-    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-    content = pdf_to_string(pdfReader)
-    pdfFileObj.close()
+    content = pdf_to_string()
     delete_created_file()
     return content
 
 
-
 # Run below to test
-path = "https://arxiv.org/pdf/1309.0238.pdf"
-pdf_to_string_process(path)
+# path = "https://arxiv.org/pdf/1309.0238.pdf"
+# pdf_to_string_process(path)

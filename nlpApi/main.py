1b573412b9bf045aa3f8291fd0a26b18f73cbbcd
Robert Nilsson <robert.nilsson.jobb@gmail.com>
2021-06-04 08:03:59 +0200

    Create articlePost endpoint in Sanic

--- nlpApi/main.py
+++ nlpApi/main.py
@@ -2,6 +2,8 @@ from sanic import Sanic, response as res
 from sanic.exceptions import NotFound
 import time
 from matcher import match_rank_articles, match_rank_articles2
+from summary import preprocessing_article, get_summary
+from pdf_to_string import pdf_to_string_process
 
 # instantiate the app object
 app = Sanic("app") # __name__
@@ -38,6 +40,25 @@ async def nlpPost(req):
     print(f"Total time in python: {end - start:.2f} s")
 
     return res.json(dictionary)
+
+
+@app.post('/articlePost')
+async def articlePost(req):
+
+    start = time.time()
+
+    article = req.json
+    article_text = pdf_to_string_process(article['path'])
+
+    article['text'] = article_text
+    article['tokentree'] = preprocessing_article(article_text)
+    article['summary'] = get_summary(article_text, 10)
+
+    end = time.time()
+    print(f"Total time in python create new article: {end - start:.2f} s")
+
+    return res.json(article)
+
 
 # start the server
 if __name__ == "__main__":
